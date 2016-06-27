b0549de98d77cdf2a624e3e8dffb6e35a92752d5
whitershade <whitershade34@gmail.com>
2016-06-27 15:33:13 +0300

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://whitershade:FufnfRhbcnb34@jello.modulusmongo.net:27017/od8uQugo' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
